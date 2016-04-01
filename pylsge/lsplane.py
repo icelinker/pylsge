2c035064b5f2730ce57613428107000675da779b
icelinker <icelinker@gmail.com>
2016-04-01 13:28:23 +0800

    Update lsplane.py

--- pylsge/lsplane.py
+++ pylsge/lsplane.py
@@ -26,34 +26,7 @@
 #% 
 #% [x0, a <, d, normd >] = lsplane(X) 
 #% --------------------------------------------------------------------- 
-# 
-#% check number of data points  
-#  m = size(X, 1); 
-#  if m < 3 
-#    error('At least 3 data points required: ' ) 
-#  end 
-#% 
-#% calculate centroid 
-#  x0 = mean(X)'; 
-#% 
-#% form matrix A of translated points 
-#  A = [(X(:, 1) - x0(1)) (X(:, 2) - x0(2)) (X(:, 3) - x0(3))]; 
-#% 
-#% calculate the SVD of A 
-#  [U, S, V] = svd(A, 0); 
-#% 
-#% find the smallest singular value in S and extract from V the 
-#% corresponding right singular vector 
-#  [s, i] = min(diag(S)); 
-#  a = V(:, i); 
-#% 
-#% calculate residual distances, if required   
-#  if nargout > 2 
-#    d = U(:, i)*s; 
-#    normd = norm(d); 
-#  end 
-#% --------------------------------------------------------------------- 
-#% End of LSPLANE.M.
+
 
 import numpy as np
 from scipy import linalg
